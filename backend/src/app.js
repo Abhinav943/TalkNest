b206414be10c80e134e5b197e1e9ae6bf4f94e06
Abhinav943 <abhinav09prajapati@gmail.com>
2026-04-26 23:36:34 +0530

    Add BorderAnimatedContainer and PageLoader components
    - Created BorderAnimatedContainer component for animated border effects.
    - Implemented PageLoader component with a spinning loader icon for loading states.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import path from 'path';
-import {connectToDB} from './lib/db.js';
-import {ENV} from "./lib/env.js";
-import cookieParser from 'cookie-parser';
-import messageRoutes from './routes/message.route.js';
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import path from "path";
+import { connectToDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 const __dirname = path.resolve();
@@ -12,26 +13,32 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-    });
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
 }
 
 (async () => {
-    try {
-        await connectToDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error('Failed to start server:', err);
-    }
+  try {
+    await connectToDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
 })();
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,10 +1,36 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-  authUser: { name: "John Doe", _id: "12345", age: 30 },
-  isLoggedIn: false,
-  login: () => {
-    console.log("Logging in...");
-    set({isLoggedIn: true});
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/protected");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.error("Error checking auth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Signup successful!");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Error signing up");
+      console.error("Error signing up:", error);
+    } finally {
+      set({ isSigningUp: false });
+    }
   },
 }));
