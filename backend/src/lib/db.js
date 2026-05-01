eb3fd6585fc72a7bf2357814389c16b7f8b40586
Abhinav943 <abhinav09prajapati@gmail.com>
2026-05-01 13:48:27 +0530

    feat: initialize project structure with ESLint config, MongoDB connection utility, and frontend entry point

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,12 @@
-import mongoose from 'mongoose';
-import {ENV} from "./env.js";
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
 export const connectToDB = async () => {
-    try{
-        const connection = await mongoose.connect(ENV.MONGO_URI);
-        console.log(`Connected to MongoDB: ${connection.connection.host}`);
-    }
-    catch(error){
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
-    }
-}
+  try {
+    const connection = await mongoose.connect(ENV.MONGO_URI);
+    console.log(`Connected to MongoDB: ${connection.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
